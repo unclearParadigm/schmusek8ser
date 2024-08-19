f3e1347298b5f25fe3515d393ca7309c6fd7fccb
Raffael Rehberger <raffael@rtrace.io>
2024-08-19 21:56:26 +0200

    use os.environ.get with default overload, instead of self-written extension

--- config_utils.py
+++ config_utils.py
@@ -1,18 +1,6 @@
 import os
 
 
-def env_var_or_default(variable_name: str, default):
-    """
-    Searches for an Environment by its name. If it is found, it's value is returned.
-    If the Environment Variable is not set, a given default value will be returned instead.
-    """
-    environment_variable = os.environ.get(variable_name)
-    if environment_variable is None:
-        return default
-
-    return environment_variable
-
-
 def list_env_var(variable_name: str) -> list:
     """
     Enumerates Environment Variables starting with a given prefix and a self-incrementing index as postfix.
@@ -30,7 +18,7 @@ def list_env_var(variable_name: str) -> list:
     found_api_keys = []
 
     while True:
-        env_var_value = env_var_or_default(f'{variable_name}__{current_index}', None)
+        env_var_value = os.environ.get(f'{variable_name}__{current_index}', None)
         if env_var_value is None:
             return found_api_keys
 
